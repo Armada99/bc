35df8105056c79197da54f502601098f213f0906
Armada99 <46710802+Armada99@users.noreply.github.com>
2020-06-20 23:29:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,8 @@ client.on('message', msg => {
     console.log(`${msg.channel.name} : ${msg.author} - ${msg.content}`);
  if (msg.content === '!ping')
      msg.reply('pong');
- /*if(msg.content.startsWith('!blob')) {
-    msg.channel.send('<a:blob:723014183645478912>');
-    msg.delete();
- }
- if(msg.content.startsWith('!pepe1')) {
-    msg.channel.send('<a:pepe_lightsaber:704821408622182470>');
-    msg.delete();
- }*/
  
- //if(msg.content.startsWith('!repeat'))
-    //for (var i=1;i<=100;i=i+1)
-    //msg.channel.send(msg.content.substring(8));
-
 if(msg.content==='!code') 
-    msg.channel.send("https://link.brawlstars.com/supportcreator/en?code=Mordeus");
-
-if(msg.content==='!check' && msg.author.equals(280745369707610114)) 
     msg.channel.send("https://link.brawlstars.com/supportcreator/en?code=Mordeus");
  
  });
