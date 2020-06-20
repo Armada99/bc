72ab3ab4df09f82c1ab2f8f3a4a59bbc924caa7e
Armada99 <46710802+Armada99@users.noreply.github.com>
2020-06-21 00:09:04 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
  const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity("Brawl Craft v1.1.4"); 
+    //client.user.setActivity("Brawl Craft v1.1.4"); 
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
