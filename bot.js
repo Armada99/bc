f2c90c9e49a54dd82566d72edce977acc17a6e8f
Armada99 <46710802+Armada99@users.noreply.github.com>
2020-06-21 11:38:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
  const client = new Discord.Client();
 
 client.on('ready', () => {
-    //client.user.setActivity("Brawl Craft v1.1.4"); 
+    client.user.setActivity("Brawl Craft v1.1.4"); 
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
-    console.log(`${msg.channel.name} : ${msg.author} - ${msg.content}`);
+    //console.log(`${msg.channel.name} : ${msg.author} - ${msg.content}`);
  if (msg.content === '!ping')
      msg.reply('pong');
  
