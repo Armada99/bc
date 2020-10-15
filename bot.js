48d3b56db44a6747abc3440057fdf4483b469777
Armada99 <46710802+Armada99@users.noreply.github.com>
2020-10-15 16:40:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,6 +65,8 @@ if(msg.member.roles.cache.has('660004539709259808') && Math.floor(Math.random()*
     if(msg.author.id=='708355911369883780') msg.react('a:rainbowblob:766186382967701505');
     //gradont
     if(msg.author.id=='245167348535787530') msg.react(':gradient:758786722187837450');
+    //ArtFromCookieLand
+    if(msg.author.id=='578996242244173844') msg.react(':cute_pancake:752623483678097469');
 }
 
 if(msg.content==='!e'&&msg.author.id==='280745369707610114'){
