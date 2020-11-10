67a09f9f16d9e96bd4036a47c5ae7b97c9966d31
Armada99 <46710802+Armada99@users.noreply.github.com>
2020-11-10 08:34:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ if(msg.content.startsWith('!repeat')&&msg.member.hasPermission("ADMINISTRATOR"))
 
 if(msg.content.startsWith('!count')&&msg.member.hasPermission("ADMINISTRATOR")){
     msg.delete();
-    for (var i=1;i<=15000;i=i+1)
+    for (var i=15001;i<=100000;i=i+1)
     msg.channel.send(i);
 }
 
